698d064e07cf02ab641da682d917310b498b1bae
Kartik Goel <goel.kartik39@gmail.com>
2018-01-13 00:11:31 +0530

    Update usersForm.php

--- usersForm.php
+++ usersForm.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\users\Form;
 
-use \Drupal\Core\Form\FormBase;
-use \Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Implementing the form.
  */
-class usersForm extends FormBase {
+class UsersForm extends FormBase {
 
   /**
    * {@inheritdoc}
@@ -20,47 +20,59 @@ class usersForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  #Fetching the details from the users
+
+  /**
+   * Fetching the details from the users.
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['users_name'] = array(
+    $form['users_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Name:'),
       '#required' => TRUE,
-    ); #Text-field for the name of the user and the title
-    $form['users_age'] = array(
+    // Text-field for the name of the user and the title.
+    ];
+    $form['users_age'] = [
       '#type' => 'number',
       '#title' => $this->t('Age:'),
       '#required' => TRUE,
-    ); #Field for the user's age and the title
-    $form['users_dob'] =array(
+    // Field for the user's age and the title.
+    ];
+    $form['users_dob'] = [
       '#type' => 'date',
       '#title' => $this->t('Date of birth:'),
       '#required' => TRUE,
-    ); #Calender for the user's date of birth and the title
-    $form['users_gender'] =array(
+    // Calender for the user's date of birth and the title.
+    ];
+    $form['users_gender'] = [
       '#type' => 'select',
       '#title' => ('Gender:'),
       '#required' => TRUE,
-      '#options'=>array(
+      '#options' => [
         'Female' => $this->t('Female'),
         'Male' => $this->t('Male'),
         'Other' => $this->t('Other'),
-      ), 
-    ); # Multiple options to choose gender from and the title
+      ],
+      // Multiple options to choose gender from and the title.
+    ];
     $form['actions']['#type'] = 'actions';
-    $form['actions']['submit'] =array(
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
       '#button_type' => 'primary',
-    ); #Submit Button
+    // Submit Button.
+    ];
     return $form;
   }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     drupal_set_message('Your Details:');
     drupal_set_message('Name: ' . $form_state->getValue('users_name'));
     drupal_set_message('Age: ' . $form_state->getValue('users_age'));
-    drupal_set_message('Date of Birth: ' . date("d-m-y",($form_state->getValue('users_dob'))));
+    drupal_set_message('Date of Birth: ' . date("d-m-y", ($form_state->getValue('users_dob'))));
     drupal_set_message('Gender: ' . $form_state->getValue('users_gender'));
-  } #Displaying the details input by the user in the fields, on the form page
- 
+  } // Displaying the details input by the user in the fields, on the form page
+
 }
